f4e1fc4f367671ede8965ff731cfec347cf3c565
Abhishek Badmaliya <abhishek.badmaliya@simformsolutions.com>
2022-05-27 10:11:12 +0530

    fix:TE5-T105, fix issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   def self.email
-    @user = User.new(user_name: "Keyur P", user_email: "keyurpatoliya21@gmail.com")
+    @user = User.new(user_name: "Keyur Patel", user_email: "keyurpatoliya21@gmail.com")
     if @user.save
       UserMailer.with(user: @user).welcome_email.deliver_now
     end
